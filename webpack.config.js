1483ec726f8a79d89d22c82aa5d5b932bfaa36b0
baeharam <hisfedev@gmail.com>
2020-04-14 15:29:24 +0900

    :tada: Init project without boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, '/dist'),
+    inline: true,
+    hot: true,
+    host: 'localhost',
+    port: 5500,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
