aa2c3db898ba1c09b2c621ca91cc06ff52e60230
baeharam <hisfedev@gmail.com>
2020-04-15 18:24:16 +0900

    :bug: Fix types

--- src/features/world/worldAPI.ts
+++ src/features/world/worldAPI.ts
@@ -1,7 +1,7 @@
 import { API_URL } from '@/utils/constants';
-import { WorldState } from './types';
+import { WorldInfo } from './types';
 
-export default async function worldAPI(): Promise<WorldState[]> {
+export default async function worldAPI(): Promise<WorldInfo[]> {
   const response = await fetch(API_URL);
   const json = await response.json();
   return json;
