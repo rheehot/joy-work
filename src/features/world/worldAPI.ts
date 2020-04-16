98c0a8c4c922abedb9e0fead3faec672981bd2e9
baeharam <hisfedev@gmail.com>
2020-04-16 16:07:42 +0900

    :bug: Change fetch into axios

--- src/features/world/worldAPI.ts
+++ src/features/world/worldAPI.ts
@@ -1,8 +1,8 @@
 import { API_URL } from '@/utils/constants';
 import { WorldInfo } from './types';
+import axios from 'axios';
 
 export default async function worldAPI(): Promise<WorldInfo[]> {
-  const response = await fetch(API_URL);
-  const json = await response.json();
-  return json;
+  const response = await axios.get(API_URL);
+  return response.data;
 }
